6aa4d3802e64908c1c0bd002cdfeefdce2be7cfa
Alex Lopez <webodessa@gmail.com>
2023-03-07 10:18:28 -0500

    its working

--- gptcli.rb
+++ gptcli.rb
@@ -1,10 +1,29 @@
+require 'dotenv/load'
 require 'httparty'
 
-url = "https://api.openapi.com/v1/completions"
+url = "https://api.openai.com/v1/completions"
 
 headers = {
-  "Authorization": "Bearer #{ENV["OPENAI_ACCESS_TOKEN"]}",
-  "Content-type": "application/json"
+  "Authorization": "Bearer #{ENV['OPENAI_ACCESS_TOKEN']}",
+  "Content-Type": "application/json"
 }
 
-p ARGV
+prompt = ARGV[0] + " .Return only the code."
+filename = ARGV[1] || "output.txt"
+
+data = {
+  model: "text-davinci-003",
+  prompt: prompt,
+  temperature: 0.2,
+  max_tokens: 300
+}
+
+response = HTTParty.post(url, headers: headers, body: data.to_json)
+
+json = JSON.parse(response.body)
+
+text = json["choices"].map { |c| c["text"] }[0]
+
+puts text
+
+File.write(filename, text)
